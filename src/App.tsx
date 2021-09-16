ae666c926c75bc0b380e53e6eac329d098461518
Austin Cory Bart <acbart@vt.edu>
2021-09-16 10:58:20 -0400

    Initial welcome App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Welcome } from './components/welcome';
 
-function App() {
+
+function App(): JSX.Element {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Welcome name="Dr. Bart" />
+        <Welcome name="Ada" />
+        <Welcome name="Babbage" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
